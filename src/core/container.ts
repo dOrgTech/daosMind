a82dd329acc7c0bc08692ead850d518b24a78c6e
Pepo <pepo.ospina@gmail.com>
2020-06-23 18:28:30 +0200

    updated uprtcl version

--- src/core/container.ts
+++ src/core/container.ts
@@ -1,4 +1,7 @@
-import { MicroOrchestrator, i18nextBaseModule } from '@uprtcl/micro-orchestrator';
+import {
+  MicroOrchestrator,
+  i18nextBaseModule,
+} from '@uprtcl/micro-orchestrator';
 import { LensesModule } from '@uprtcl/lenses';
 import { DocumentsModule } from '@uprtcl/documents';
 import { WikisModule } from '@uprtcl/wikis';
@@ -16,26 +19,54 @@ import { DiscoveryModule } from '@uprtcl/multiplatform';
 
 import { SimpleWiki } from './simple-wiki';
 
-type version = 1 | 0
+type version = 1 | 0;
 export class WikiContainer {
   private c1host = 'https://api.intercreativity.io/uprtcl/1';
+  //private c1host = 'http://localhost:3100/uprtcl/1';
   private ethHost = '';
-  private ipfsConfig = { host: 'ipfs.intercreativity.io', port: 443, protocol: 'https' };
+  private ipfsConfig = {
+    host: 'ipfs.intercreativity.io',
+    port: 443,
+    protocol: 'https',
+  };
 
-  private httpCidConfig = { version: 1 as version, type: 'sha3-256', codec: 'raw', base: 'base58btc' };
-  private ipfsCidConfig = { version: 1 as version, type: 'sha2-256', codec: 'raw', base: 'base58btc' };
+  private httpCidConfig = {
+    version: 1 as version,
+    type: 'sha3-256',
+    codec: 'raw',
+    base: 'base58btc',
+  };
+  private ipfsCidConfig = {
+    version: 1 as version,
+    type: 'sha2-256',
+    codec: 'raw',
+    base: 'base58btc',
+  };
+
+  private orchestrator = new MicroOrchestrator();
 
   private httpConnection = new HttpConnection();
   private ethConnection = new EthereumConnection({ provider: this.ethHost });
-  
-  private httpEvees = new EveesHttp(this.c1host, this.httpConnection, this.ethConnection, this.httpCidConfig);
-  private ethEvees = new EveesEthereum(this.ethConnection, this.ipfsConfig, this.ipfsCidConfig);
 
-  private evees = new EveesModule([this.ethEvees, this.httpEvees], this.httpEvees);
+  private httpEvees = new EveesHttp(
+    this.c1host,
+    this.httpConnection,
+    this.ethConnection,
+    this.httpCidConfig
+  );
+  private ethEvees = new EveesEthereum(
+    this.ethConnection,
+    this.ipfsConfig,
+    this.ipfsCidConfig,
+    this.orchestrator.container
+  );
+
+  private evees = new EveesModule(
+    [this.ethEvees, this.httpEvees],
+    this.httpEvees
+  );
   private documents = new DocumentsModule();
   private wikis = new WikisModule();
-  
-  private orchestrator = new MicroOrchestrator();
 
   async initializeMicroOrchestrator(web3provider, dispatcher, hasHomeProposal) {
     const modules = [
@@ -47,12 +78,15 @@ export class WikiContainer {
       new AccessControlModule(),
       this.evees,
       this.documents,
-      this.wikis
+      this.wikis,
     ];
 
     try {
       await this.orchestrator.loadModules(modules);
-      customElements.define('simple-wiki', SimpleWiki(web3provider, dispatcher, hasHomeProposal));
+      customElements.define(
+        'simple-wiki',
+        SimpleWiki(web3provider, dispatcher, hasHomeProposal)
+      );
     } catch (e) {
       console.log(e);
     }
@@ -65,6 +99,9 @@ export class WikiContainer {
   private static _instance: WikiContainer;
 
   public static getInstance(web3provider, dispatcher, hasHomeProposal) {
-    return this._instance || (this._instance = new this(web3provider, dispatcher, hasHomeProposal));
+    return (
+      this._instance ||
+      (this._instance = new this(web3provider, dispatcher, hasHomeProposal))
+    );
   }
 }
